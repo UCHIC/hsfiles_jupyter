6dd3b7a1d818388adc0ace66e1e101c40f3d7c45
pkdash <pabitra.dash@usu.edu>
2025-08-14 16:21:58 -0400

    refactor download_file_from_hydroshare and list_available_files_for_download to include base_path parameter for improved directory handling and path validation

--- hsfiles_jupyter/download_file.py
+++ hsfiles_jupyter/download_file.py
@@ -4,18 +4,20 @@ from .utils import (
     FileCacheUpdateType,
     HydroShareAuthError,
     ResourceFileCacheManager,
+    get_hydroshare_resource_download_dir,
     get_local_absolute_file_path,
     logger,
 )
 
 
-async def download_file_from_hydroshare(resource_id: str, hs_file_path: str):
+async def download_file_from_hydroshare(resource_id: str, hs_file_path: str, base_path: str):
     """
     Downloads a file from HydroShare resource to the local Jupyter environment.
 
     Args:
         resource_id: The ID of the HydroShare resource
-        hs_file_path: The path of the file in HydroShare (relative to resource's data/contents)
+        hs_file_path: The path of the file in HydroShare (relative to resource's data/contents/)
+        base_path: The base path where the resource id folder exists (e.g., "Downloads", "work/Downloads", etc.)
 
     Returns:
         A dictionary with success or error message
@@ -25,10 +27,15 @@ async def download_file_from_hydroshare(resource_id: str, hs_file_path: str):
     try:
         # Get the resource
         resource = rfc_manager.get_resource(resource_id)
-    except HydroShareAuthError as e:
+    except (HydroShareAuthError, ValueError) as e:
         return {"error": str(e)}
-    except ValueError as e:
-        return {"error": str(e)}
+
+    hs_download_dir = get_hydroshare_resource_download_dir()
+    base_path = base_path.rstrip("/")
+    # check the base path is the hydroshare download directory where nbfetch downloads the resources
+    if base_path != hs_download_dir:
+        return {"error": "Select the option to download from within the resource id folder in the HydroShare download"
+        " directory."}
 
     # Get all files in the resource
     files, _ = rfc_manager.get_files(resource, refresh=True)
@@ -38,8 +45,11 @@ async def download_file_from_hydroshare(resource_id: str, hs_file_path: str):
         err_msg = f"File {hs_file_path} is not found in HydroShare resource: {resource_id}"
         return {"error": err_msg}
 
-    # Construct the local file path
-    local_dir_path = f"Downloads/{resource_id}/data/contents"
+    # Construct the local file path using the provided base_path
+    local_dir_path = f"{base_path}/{resource_id}/data/contents"
+    # check the resource directory exists
+    if not os.path.exists(get_local_absolute_file_path(local_dir_path)):
+        return {"error": f"Local directory {local_dir_path} does not exist"}
     local_file_path = os.path.join(local_dir_path, hs_file_path)
 
     # Ensure the directory exists
@@ -69,12 +79,13 @@ async def download_file_from_hydroshare(resource_id: str, hs_file_path: str):
         return {"error": err_msg}
 
 
-async def list_available_files_for_download(resource_id: str):
+async def list_available_files_for_download(resource_id: str, base_path: str):
     """
     Lists files in a HydroShare resource that are not downloaded locally.
 
     Args:
         resource_id: The ID of the HydroShare resource
+        base_path: The base path where the resource id folder exists (e.g., "Downloads", "work/Downloads", etc.)
 
     Returns:
         A dictionary with a list of files available for download or an error message
@@ -89,11 +100,19 @@ async def list_available_files_for_download(resource_id: str):
     except ValueError as e:
         return {"error": str(e)}
 
+    hs_download_dir = get_hydroshare_resource_download_dir()
+    base_path = base_path.rstrip("/")
+    # check the base path is the hydroshare download directory where nbfetch downloads the resources
+    if base_path != hs_download_dir:
+        err_msg = f"Select the option to download from within the resource id:{resource_id} folder in the HydroShare" \
+        f" downlaod directory: {hs_download_dir}"
+        return {"error": err_msg}
+
     # Get all files in the resource
     remote_files, _ = rfc_manager.get_files(resource, refresh=True)
 
-    # Get the local directory path
-    local_dir_path = f"Downloads/{resource_id}/data/contents"
+    # Get the local directory path using the provided base_path
+    local_dir_path = f"{base_path}/{resource_id}/data/contents"
     local_abs_dir_path = get_local_absolute_file_path(local_dir_path)
 
     # Check which files exist locally
