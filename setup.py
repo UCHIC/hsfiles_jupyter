9bd338b27d8661508bd8e60f2146a87bf8171401
pkdash <pabitra.dash@usu.edu>
2025-08-21 10:03:54 -0400

    bump version to 0.2.1 for release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LABEXTENSION_PATH = os.path.join(HERE, PKG_NAME, "labextension")
 
 setup(
     name=PKG_NAME,
-    version="0.2.0",
+    version="0.2.1",
     author="Pabitra Dash",
     author_email="pabitra.dash@usu.edu",
     description="A JupyterLab extension to manage HydroShare resource files in JupyterLab",
